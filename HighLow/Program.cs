879881ce602f4d3a8136c9dd0dbb55f3a8ddf959
chajus1 <chajus1@googlemail.com>
2024-09-23 19:18:49 +0100

    Add project files.

---
+++ HighLow/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HighLow
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to High Low.");
+            Console.WriteLine($"Guess numbers between 1 and {HighLow.MAXIMUM}.");
+            HighLow.Hint();
+            while(HighLowGame.GetPot() > 0)
+            {
+                Console.WriteLine("Press h for higher, l for lower, ? to buy a hint,");
+                Console.WriteLine($"or any other key to quit with {HighLowGame.GetPot()}.");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == 'h') HighLowGame.Guess(true);
+                else if (key == 'l') HighLowGame.Guess(false);
+                else if (key == '?') HighLowGame.Hint();
+                else return;                    
+            }
+            Console.WriteLine("The pot is empty. Bye!");
+        }
+    }
+}
